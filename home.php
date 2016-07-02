9cfc41fb0b83dc165b66df6349a0f39efd799c1c
Digital-Storm <Digital-Storm@users.noreply.github.com>
2016-07-01 21:07:23 -0400

    Replace old home.php with modified version
    Modified version supports transmission and qbittorrent, and dynamic removal of content.

--- home.php
+++ home.php
@@ -1,204 +1,140 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Main</title>
-
-    <!-- Bootstrap CSS -->    
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- bootstrap theme -->
-    <link href="css/bootstrap-theme.css" rel="stylesheet">
-    <!--external css-->
-    <!-- font icon -->
-    <link href="css/elegant-icons-style.css" rel="stylesheet" />
-    <link href="css/font-awesome.min.css" rel="stylesheet" />    
-    <!-- Custom styles -->
-	<link href="css/widgets.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/style-responsive.css" rel="stylesheet" />
-	<link href="css/jquery-ui-1.10.4.min.css" rel="stylesheet">
-  </head>
- <body>
- <?php
-        
-	// Process requests
-	include 'requests.php';
-	//$cfg is loaded from requests.php      
-	//$torrents = getRequest("/query/torrents?filter=all&sort=name");
-        //$global_info = getRequest("/query/transferInfo");
-        $show_errors = $cfg['show_errors'];
-        if( $cfg['show_weather'])
-        {
-            $weather = weather();
-        }    
-        header('Refresh: ' . $cfg['refresh_seconds']);
- ?>
-     
-  <!--main content start-->
-      <section id="main-content">
-          <section class="wrapper-frame">            
-              <!--overview start-->
-			  <div class="row">	
-			  		<div class="col-lg-6">
-					<h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
-					</div>
-					<div class="col-1">
-                                            <?php if($cfg['show_weather']){ ?>
-						<canvas id="weather-canvas" width="64" height="64"></canvas>
-						<script src="js/skycons.js"></script>
-						<script>						
-						var icons = new Skycons();
-						icons.set("weather-canvas", "<?php echo $weather[1];?>");
-						icons.play();
-						</script>						
-                                            <?php } ?>
-					</div>
-					<div class="col-5">
-                                        <?php if($cfg['show_weather']){ ?>
-					<h3 class="page-header"><?php echo "<b>" . round($weather[2]) . "</b>, " . $weather[0] . ",  Feels like <b>" . round($weather[3]) . "</b>";?></h3>		
-                                        <?php } ?>
-					</div>
-			  </div>
-			  <div class="row">				
-				<div class="col-lg-12">
-					<ol class="breadcrumb">
-						<li><i class="fa fa-home"></i><a href="home.php">Home</a></li>
-						<li><i class="fa fa-laptop"></i>Dashboard</li>						  	
-					</ol>
-				</div>
-			</div>
-              
-            <div class="row">			
-				<div class="col-lg-6 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box blue-bg">
-                                            <h3>Downloading <span class="speeds"><?php echo round(intval($global_info['dl_info_speed'])/1000000, 2) . " mB/s"; ?></span></h3>	
-						<?php
-                                                if($torrents == null){
-                                                    echo "There was a problem fetching torrents.";                                                    
-                                                }
-                                                else {
-                                                    foreach($torrents as $torrent)
-                                                    {
-    							if($torrent['state'] == "downloading")
-                                                            {
-    								echo $torrent['name'] . "<br>";
-                                                            }
-                                                    }
-                                                }
-                                                ?>
-					</div><!--/.info-box-->			
-				</div><!--/.col-->
-
-				<div class="col-lg-6 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box green-bg">	
-						<h3>Seeding <span class="speeds"><?php echo round(intval($global_info['up_info_speed'])/1000000, 2) . " mB/s"; ?></span></h3>
-						<?php
-                                                if($torrents == null){
-                                                    echo "There was a problem fetching torrents.";                                                    
-                                                }
-                                                else {
-                                                    foreach($torrents as $torrent)
-                                                    {
-    							if($torrent['state'] == "uploading")
-                                                            {
-    								echo $torrent['name'] . "<br>";
-                                                            }
-                                                    }
-                                                }    
-						?>
-					</div><!--/.info-box-->			
-				</div><!--/.col-->
-				
-			</div><!--/.row-->	
-
-			<div class="row">
-				<div class="col-lg-12 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box-prtg white-bg">
-					<?php					
-					echo "<iframe width=100% height=100% frameborder=\"0\" src=\"" . $cfg['prtg_map'] .  "\"></iframe>";
-					?>
-					</div>
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="col-lg-12 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box all-bg">
-					<h3>All</h3>
-						<?php
-                                                if($torrents == null){
-                                                    echo "There was a problem fetching torrents.";                                                    
-                                                }
-                                                else {
-                                                    foreach($torrents as $torrent)
-                                                    {							
-                                                            $ratio = $torrent['ratio'];
-                                                            if(strlen($ratio) > 4) 
-                                                            {
-    								$ratio = substr($ratio, 0, 4);
-                                                            }
-                                                            echo $torrent['name'] . "<span style=\"color: orange; font-weight: bold;\"> | Ratio: " . $ratio . "</span><br>";							
-                                                    }
-                                                }
-						?>
-					</div>
-				</div>
-			</div>                        
-                        <?php
-                        // Error display
-                        if($show_errors && count($errors) > 0)
-                        { 
-                            echo "<div class=\"row\">";
-                            echo "<div class=\"col-lg-12 col-md-3 col-sm-12 col-xs-12\">";
-                            echo "<div class=\"info-box red-bg\">";
-                            echo "<h3><span style=\"color: black; font-weight: bold;\">Errors</span></h3>";
-                            foreach($errors as $err){
-                                echo "<span style=\"color: black; font-weight: bold;\">" . $err . "</span><br>";
-                            }
-                            echo "</div>";
-                            echo "</div>";
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Main</title>
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- bootstrap theme -->
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+        <!--external css-->
+        <!-- font icon -->
+        <link href="css/elegant-icons-style.css" rel="stylesheet" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" />
+        <!-- Custom styles -->
+        <link href="css/widgets.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/style-responsive.css" rel="stylesheet" />
+        <link href="css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php
+            // Process requests
+            include 'requests.php';
+            //$cfg is loaded from requests.php
+            $client = strtolower($cfg['torrent_client']);
+            if( $client == "qbittorrent"){
+                $torrents = getRequest("/query/torrents?filter=all&sort=name");
+                $global_info = getRequest("/query/transferInfo");
+            } elseif ($client == "transmission"){
+                $torrents = transmissionTorrents();
+    	        $global_info = transmissionAll();
+            }
+            //Show Errors?
+            $show_errors = $cfg['show_errors'];
+            //Show Weather
+            if( $cfg['show_weather']){
+                $weather = weather();
+            }
+            header('Refresh: ' . $cfg['refresh_seconds']);
+        ?>
+    
+        <!--main content start-->
+        <section id="main-content">
+            <!--overview start-->
+            <section class="wrapper-frame">
+                <!--Dashboard and Weather Row-->
+                <!--Dashboard section-->
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
+                    </div>
+                    <!--Weather Section-->
+                    <div class="col-1">
+                        <?php if($cfg['show_weather']){ ?>
+                            <canvas id="weather-canvas" width="64" height="64"></canvas>
+                            <script src="js/skycons.js"></script>
+                            <script>
+                                var icons = new Skycons();
+                                icons.set("weather-canvas", "<?php echo $weather[1];?>");
+                                icons.play();
+                            </script>
+                        <?php } ?>
+                    </div>
+                    <div class="col-5">
+                        <?php if($cfg['show_weather']){ ?>
+                            <h3 class="page-header"><?php echo "<b>" . round($weather[2]) . "</b>, " . $weather[0] . ",  Feels like <b>" . round($weather[3]) . "</b>";?></h3>
+                        <?php } ?>
+                    </div>
+                </div>
+                <!--Breadcrumb Section-->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <ol class="breadcrumb">
+                            <li><i class="fa fa-home"></i><a href="home.php">Home</a></li>
+                            <li><i class="fa fa-laptop"></i>Dashboard</li>
+                        </ol>
+                    </div>
+                </div>
+                <!--Downloading and Seeding Section-->
+                <?php
+                    if (!$cfg['torrent_client'] == ""){
+                        include 'torrent.php';
+                    }
+                ?>
+                <!--prtg section-->
+                <?php
+                    if ($cfg['show_prtg']){
+                        include 'prtg.php';
+                    }
+                ?>
+                <!--Torrent List section-->
+                <?php
+                    if (!$cfg['torrent_client'] == ""){
+                        include 'torrents.php';
+                    }
+                ?>
+                <!--Error display-->
+                <?php
+                    if($show_errors && count($errors) > 0){
+                        echo "<div class=\"row\">";
+                        echo "<div class=\"col-lg-12 col-md-3 col-sm-12 col-xs-12\">";
+                        echo "<div class=\"info-box red-bg\">";
+                        echo "<h3><span style=\"color: black; font-weight: bold;\">Errors</span></h3>";
+                        foreach($errors as $err){
+                            echo "<span style=\"color: black; font-weight: bold;\">" . $err . "</span><br>";
                         }
-                        ?>
- 
-			
-           </div>  
-            
-		  
-		  <!-- Today status end -->
-			
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                ?>
+            </section>
+            <!--overview end-->
+        </section>
+        <!--main content end-->
 
-          </section>
-      </section>
-      <!--main content end-->
-  </section>
-  <!-- container section start -->
-
-    <!-- javascripts -->
-    <script src="js/jquery.js"></script>
-	<script src="js/jquery-ui-1.10.4.min.js"></script>
-    <script src="js/jquery-1.8.3.min.js"></script>
-    <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
-    <!-- bootstrap -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- nice scroll -->
-    <script src="js/jquery.scrollTo.min.js"></script>
-    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-
-    <!-- custom select -->
-    <script src="js/jquery.customSelect.min.js" ></script>
-   
-    <!--custome script for all page-->
-    <script src="js/scripts.js"></script>
-    <!-- custom script for this page-->
-	<script src="js/jquery.autosize.min.js"></script>
-	<script src="js/jquery.placeholder.min.js"></script>
-	<script src="js/jquery.slimscroll.min.js"></script>
-	
-	<!--weather scripts-->
-	<script src="js/skycons.js"></script>
-
- 
-  </body>
-  </html>
+        <!-- container section start -->
+        <!-- javascripts -->
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery-ui-1.10.4.min.js"></script>
+        <script src="js/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
+        <!-- bootstrap -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- nice scroll -->
+        <script src="js/jquery.scrollTo.min.js"></script>
+        <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+        <!-- custom select -->
+        <script src="js/jquery.customSelect.min.js" ></script>
+        <!--custome script for all page-->
+        <script src="js/scripts.js"></script>
+        <!-- custom script for this page-->
+        <script src="js/jquery.autosize.min.js"></script>
+        <script src="js/jquery.placeholder.min.js"></script>
+        <script src="js/jquery.slimscroll.min.js"></script>
+        <!--weather scripts-->
+        <script src="js/skycons.js"></script>
+    </body>
+</html>
